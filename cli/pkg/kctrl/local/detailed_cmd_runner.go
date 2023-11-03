484e2f53247af2b7961f2b7b1a89126ed0ac1583
rcmadhankumar <rcmadhankumar@gmail.com>
2023-11-03 13:57:22 +0530

    Updated command runner to use tanzu binaries
    Signed-off-by: rcmadhankumar <rcmadhankumar@gmail.com>

--- cli/pkg/kctrl/local/detailed_cmd_runner.go
+++ cli/pkg/kctrl/local/detailed_cmd_runner.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	goexec "os/exec"
+	"strings"
 
 	"github.com/vmware-tanzu/carvel-kapp-controller/pkg/exec"
 )
@@ -38,6 +39,35 @@ func (r DetailedCmdRunner) RunWithCancel(cmd *goexec.Cmd, cancelCh chan struct{}
 	if r.fullOutput {
 		cmd.Stdout = io.MultiWriter(r.log, cmd.Stdout)
 		cmd.Stderr = io.MultiWriter(r.log, cmd.Stderr)
+	}
+
+	if strings.Contains(cmd.Args[0], "kapp") {
+		fmt.Printf("Here's the kapp command")
+		// Edit the command (change the command and its arguments)
+		fmt.Printf("\n ====== > cmd.path %+v", cmd.Path)
+		fmt.Printf("\n ====== > cmd.args %+v", cmd.Args)
+		fmt.Printf("\n ====== > cmd.Env %+v", cmd.Env)
+
+		path, err := goexec.LookPath("tanzu")
+		if err != nil {
+			fmt.Printf("Error: %+v", err)
+		}
+
+		fmt.Printf("\n Lookpath : %s", path)
+
+		modifiedCmd := &goexec.Cmd{
+			Path:   path,
+			Args:   append([]string{"tanzu"}, cmd.Args...),
+			Env:    cmd.Env, // Copy the environment variables
+			Stdin:  cmd.Stdin,
+			Stdout: cmd.Stdout,
+			Stderr: cmd.Stdout,
+		}
+
+		fmt.Fprintf(r.log, "==> Executing %s %v\n", cmd.Path, cmd.Args)
+		defer fmt.Fprintf(r.log, "==> Finished executing %s\n\n", cmd.Path)
+		return exec.PlainCmdRunner{}.RunWithCancel(modifiedCmd, cancelCh)
+
 	}
 
 	fmt.Fprintf(r.log, "==> Executing %s %v\n", cmd.Path, cmd.Args)
